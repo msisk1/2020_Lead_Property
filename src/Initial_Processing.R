8cef3f77f8bae89b8c8fe0767264259acceaf336
pmcguir2 <79151771+pmcguir2@users.noreply.github.com>
2021-03-01 15:14:21 -0500

    Unique Kit IDs, non-combined Google Sheet, and ignoring API/csv file

--- src/Initial_Processing.R
+++ src/Initial_Processing.R
@@ -1,37 +1,49 @@
 library(googlesheets4)
 sheet.gid <- "1gzDR0CGf-kYddJZS7koD1iYwQQNKSiS1xdQ7AB0rxSo"
 
+kits.2020 <- read_sheet(sheet.gid, sheet = "2020")
+kits.2021 <- read_sheet(sheet.gid, sheet = "2021")
 
-kits.combined <- read_sheet(sheet.gid, sheet = "Combined")
+kits.combined <- rbind(kits.2019, kits.2020, kits.2021)
+
+
+kits.combined.unique <- kits.combined[!duplicated(kits.combined$`Kit ID`), ]
+
+kits.ready <- kits.combined.unique[!is.na(kits.combined.unique$Address), ]
+
+kits.ready<-kits.ready[!(kits.ready$Address=="21506" | kits.ready$Address=="19391" | kits.ready$Address=="No Form Provided"),]
+
+
 
 if(!requireNamespace("devtools")) install.packages("devtools")
 devtools::install_github("dkahle/ggmap")
 library("ggmap")
 
-register_google(key = "___")
+source("API.R")
+print(API)
+register_google(key = API)
 
 
-for(i in 1:nrow(kits.combined)){
-  result <- geocode(as.character(kits.combined$Address[i]), output = "latlona", source = "google")
+for(i in 1:nrow(kits.ready)){
+  result <- geocode(as.character(kits.ready$Address[i]), output = "latlona", source = "google")
+  
   while(is.na(result[1])){ #checks if the latitude is NA and reruns if it is
     Sys.sleep(2) #Pauses for a minute to let the API Catch up
-    result <- geocode(as.character(kits.combined$Address[i]), output = "latlona", source = "google")
+    result <- geocode(as.character(kits.ready$Address[i]), output = "latlona", source = "google")
+    
   } 
-  kits.combined$lon[i] <- as.numeric(result[1])
-  kits.combined$lat[i] <- as.numeric(result[2])
-  kits.combined$geoAddress[i] <- as.character(result[3])
+  kits.ready$lon[i] <- as.numeric(result[1])
+  kits.ready$lat[i] <- as.numeric(result[2])
+  kits.ready$geoAddress[i] <- as.character(result[3])
 }
-head(kits.combined)
+head(kits.ready)
 
-kits.combined[is.na(kits.combined)] <- "N/A"
+kits.ready <- apply(kits.ready,2,as.character)
 
-kits.combined <- apply(kits.combined,2,as.character)
+write.csv(kits.ready, 'kits_ready.csv', na="-")
 
-write.csv(kits.combined, '/Users/patrick.mcguire/Downloads/kits_combined.csv')
-
-final.kits <- read.csv('/Users/patrick.mcguire/Downloads/kits_combined.csv')
+final.kits <- read.csv('kits_ready.csv')
 
 sheet_write(final.kits, ss = "1gzDR0CGf-kYddJZS7koD1iYwQQNKSiS1xdQ7AB0rxSo", sheet = "Combined + Geocoded")
 
 install.packages("maptools")
-
