aabc7361570a6597d63f2fe63c73d691ebcb7746
pmcguir2 <79151771+pmcguir2@users.noreply.github.com>
2021-04-20 09:01:04 -0400

    Update Initial_Processing.R

--- src/Initial_Processing.R
+++ src/Initial_Processing.R
@@ -55,6 +55,7 @@ for(i in 1:nrow(kits.ready)){
 head(kits.ready)
 
 library(dplyr)
+library(tidyr)
 
 kits.ready %>% select(-testlon)
 
@@ -63,8 +64,56 @@ kits.ready <- apply(kits.ready,2,as.character)
 write.csv(kits.ready, 'kits_ready.csv', na="-")
 
 final.kits <- read.csv('kits_ready.csv')
+final.kits.no.na <- drop_na(final.kits)
 
 sheet_write(final.kits, ss = "1gzDR0CGf-kYddJZS7koD1iYwQQNKSiS1xdQ7AB0rxSo", sheet = "Combined + Geocoded")
+
+library (sf)
+parcels = st_read("src/sbparcels")
+parcels <- st_transform(parcels, crs=4326)
+final.kits.sf = st_as_sf(final.kits.no.na, coords = c(x = "lon", y = "lat"), crs = 4326)
+
+parcel_join <- st_join(final.kits.sf, parcels, join = st_nearest_feature)
+as.data.frame(parcel_join)
+parcel_join$conyear <- NA
+
+kit_parcel <- subset(parcel_join, select = c("Kit.ID", "PARCELID"))
+
+library(XML)
+library(RCurl)
+library(rlist)
+
+sid = "1944116155AE43FBA68587F1F8294D9C"
+
+for(i in 1:nrow(parcel_join)){
+  url.overview <- paste0("http://in-stjoseph-assessor.governmax.com/propertymax/ACAMA_INDIANA/tab_improve_v0704.asp?t_nm=improvements&l_cr=5&t_wc=|parcelid=",
+                       parcel_join$PARCELSTAT[i],
+                       "&sid=",
+                       sid)
+  print(url.overview)
+  theurl <- getURL(url.overview,.opts = list(ssl.verifypeer = FALSE) )
+  library(xml2)
+  each.html <- read_html(url.overview)
+  write_html(each.html,"file.html",encoding = "UTF-8") 
+  tables <- readHTMLTable("file.html")
+  #try without caching 
+  tables <- list.clean(tables, fun = is.null, recursive = FALSE)
+  q <- as.data.frame(tables[2])
+  
+  library(tidyverse)
+  q <- q[ ,4:ncol(q)] %>% na.omit()
+  if (nrow(q) < 2){
+      print (paste0 (parcel_join$PARCELSTAT[i], " is vacant"))
+  }else{
+      var.names <- c("Improvement Type Code", 	"Building No.", 	"ID No.", 	"Constructed Year", "Grade" 	,"Total Base Area","Replacement Cost")
+      names(q)<-var.names
+      parcel_join$conyear[i] <- as.numeric(min(q$`Constructed Year`))
+  }
+}
+
+
+
+
 
 library (leaflet)
 library (sp)
@@ -81,47 +130,5 @@ m <- leaflet() %>%
 
 m
 
-library (sf)
-parcels = st_read("src/sbparcels")
-parcels <- st_transform(parcels, crs=4326)
-final.kits.sf = st_as_sf(final.kits, coords = c(x = "lon", y = "lat"), crs = 4326)
+head(kits.ready)
 
-parcel_join <- st_join(final.kits.sf, parcels, join = st_nearest_feature)
-
-kit_parcel <- subset(parcel_join, select = c("Kit.ID", "PARCELID"))  
-
-rm(list=ls(all=TRUE)) # clear memory
-
-library(rvest)
-library (textreadr)
-library(tidyverse)
-library(xml2)
-
-sid = "4EDA38D4D6CA4BABBDA499D4CFB99F14"
-
-# testing for a url I know works, but only produces "null"- is it not reading the data properly?
-
-url<- paste0("http://in-stjoseph-assessor.governmax.com/propertymax/ACAMA_INDIANA/tab_improve_v0704.asp?t_nm=improvements&l_cr=5&t_wc=|parcelid=", parcel_join$PARCELSTAT[5],"&sid=", sid)
-each.html <- read_html(url)
-write_html(each.html,"file.html",encoding = "UTF-8")
-full.parsed.tables <- each.html %>% 
-  html_table(fill = T)
-
-for(i in parcel_join) {
-  url[i]<- paste0("http://in-stjoseph-assessor.governmax.com/propertymax/ACAMA_INDIANA/tab_improve_v0704.asp?t_nm=improvements&l_cr=5&t_wc=|parcelid=", parcel_join$PARCELSTAT[i],"&sid=", sid)
-  each.html <- read_html(url[i])
-  write_html(each.html,"file.html",encoding = "UTF-8")
-  
-  full.parsed.tables <- each.html %>% 
-  html_table(fill = T)
-  
-  each.imp.table <- as.data.frame(full.parsed.tables[13]) %>% #the 13 seems to pull the proper section for all I have done
-  select(-c(X1,X2,X3))%>% #tossing some unneded values
-  filter_all(any_vars(!is.na(.))) #removing empty rows
-  names(each.imp.table) <- each.imp.table[2,] 
-  each.imp.table <- data.frame(each.imp.table[3:nrow(each.imp.table),])
-  each.assessed.table <- as.data.frame(full.parsed.tables[11]) %>% #the 11 seems to pull the assessment
-  select(-c(X2,X6,X7))%>% #tossing some unneded values
-  filter_all(any_vars(!is.na(.))) #removing empty rows
-  assessed.value <- each.assessed.table[2,3]
-}
